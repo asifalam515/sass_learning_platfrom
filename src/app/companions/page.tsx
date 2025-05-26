4a0d995180c69116de8341397e081797f055884c
asifalam515 <asibulalam515@gmail.com>
2025-05-26 15:14:22 +0600

    dynamic nav active applied using ternary operator

--- src/app/companions/page.tsx
+++ src/app/companions/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const CompanionsLibary = () => {
-  return <div></div>;
+  return <div>Companions</div>;
 };
 
 export default CompanionsLibary;
--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,4 +1,8 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { Herr_Von_Muellerhoff } from "next/font/google";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 const navItems = [
   { label: "Home", href: "/" },
@@ -6,10 +10,15 @@ const navItems = [
   { label: "My Journey", href: "/my-journey" },
 ];
 const NavItems = () => {
+  const pathName = usePathname();
   return (
     <nav className="flex items-center gap-4">
       {navItems.map(({ label, href }) => (
-        <Link href={href} key={label}>
+        <Link
+          href={href}
+          key={label}
+          className={pathName === href ? "text-primary font-semibold" : ""}
+        >
           {" "}
           {label}{" "}
         </Link>
