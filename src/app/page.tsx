4654648ef44a9413ffed2ce8400fa2e93e8eddc6
asifalam515 <asibulalam515@gmail.com>
2025-06-09 00:43:39 +0600

    updated type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import CompanionCard from "@/components/CompanionCard";
 import CompanionList from "@/components/CompanionList";
 import CTA from "@/components/CTA";
 import { Button } from "@/components/ui/button";
+import { recentSessions } from "@/constants";
 import React from "react";
 
 const Page = () => {
@@ -36,7 +37,11 @@ const Page = () => {
         ></CompanionCard>
       </section>
       <section className="home-section">
-        <CompanionList></CompanionList>
+        <CompanionList
+          title="Recently Completed Sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        ></CompanionList>
         <CTA></CTA>
       </section>
     </main>
--- src/components/CompanionList.tsx
+++ src/components/CompanionList.tsx
@@ -1,7 +1,47 @@
 import React from "react";
-
-const CompanionList = () => {
-  return <div>CompanionList</div>;
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+interface CompanionListProps {
+  title: string;
+  companions: string;
+  classNames: string;
+}
+const CompanionList = ({
+  title,
+  companions,
+  classNames,
+}: CompanionListProps) => {
+  return (
+    <article>
+      <h2>Recent sessions</h2>
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Invoice</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Method</TableHead>
+            <TableHead className="text-right">Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell className="text-right">$250.00</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </article>
+  );
 };
 
 export default CompanionList;
