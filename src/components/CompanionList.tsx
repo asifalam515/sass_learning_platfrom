9d5119cb60463323cb3baae7157990991a3c9629
asifalam515 <asibulalam515@gmail.com>
2025-06-12 23:02:45 +0600

    subject rendered

--- src/components/CompanionList.tsx
+++ src/components/CompanionList.tsx
@@ -41,15 +41,27 @@ const CompanionList = ({
                   <div className="flex items-center gap-2">
                     <div className="size-[72px] flex itmes-center justify-center rounded-lg max-md:hidden ">
                       <Image
-                        src={`/icons/${companion.subject}.svg`}
+                        src={`src/public/icons/${companion.subject}.svg`}
                         alt="image"
                         width={35}
                         height={35}
                       ></Image>
                     </div>
+
+                    <div className="flex flex-col gap-2">
+                      <p className="font-bold">{companion.name}</p>
+                      <p className="text-lg"> {companion.topic} </p>
+                    </div>
                   </div>
                   {companion.subject}
                 </Link>
+              </TableCell>
+              <TableCell>
+                <div className="subject-badge w-fit">{companion.subject}</div>
+                <div
+                  className="flex items-center justify-center rounded-lg w-fit p-2 md:hidden"
+                  style={{}}
+                ></div>
               </TableCell>
             </TableRow>
           ))}
